759bd51eb7485a64cb1fab4f3bf90ead29d4348b
Timofey Latypov <90103780+N0tilT@users.noreply.github.com>
2023-11-24 23:34:49 +0300

    initial

---
+++ ConsoleApp1/ClassLibrary1/Translator.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary1
+{
+    public class Translator
+    {
+        public Translator() { }
+
+        public void Print() { Console.WriteLine("Hello"); }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,5 @@
+﻿using ClassLibrary1;
+
+Translator translator = new Translator();
+
+translator.Print();
